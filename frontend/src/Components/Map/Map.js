b4071b791593b3c74c03454978b1cad9db06bb45
Владимир Орешин <VSOreshin@sberbank.ru>
2021-10-07 16:30:04 +0300

    added geoJSON

--- frontend/src/Components/Map/Map.js
+++ frontend/src/Components/Map/Map.js
@@ -1,6 +1,10 @@
 import React from "react";
-import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import {MapContainer, TileLayer, GeoJSON, Marker, Popup, useMapEvents} from 'react-leaflet';
 import useStyles from "./style";
+
+/**
+ * @return {null}
+ */
 
 function Map(props) {
   const classes = useStyles();
@@ -8,11 +12,12 @@ function Map(props) {
 
   return (
     <div className={classes.container}>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true} className={classes.containerMap}>
+      <MapContainer center={[55.705, 37.59]} zoom={10} scrollWheelZoom={true} className={classes.containerMap}>
         <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://tile2.maps.2gis.com/tiles?z={z}&x={x}&y={y}&v=1"
         />
+        <GeoJSON data={data}/>
         <Marker position={[51.505, -0.09]}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
