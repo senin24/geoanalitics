478980e78ad56ddb79804b158d30203d23e51ebc
Владимир Орешин <VSOreshin@sberbank.ru>
2021-10-07 20:23:07 +0300

    added layer control, change position zoom control

--- frontend/src/Components/Map/Map.js
+++ frontend/src/Components/Map/Map.js
@@ -1,28 +1,65 @@
-import React from "react";
-import {MapContainer, TileLayer, GeoJSON, Marker, Popup, useMapEvents} from 'react-leaflet';
+import React, {useEffect} from "react";
+import {MapContainer, TileLayer, GeoJSON, Marker, Popup, LayersControl, ZoomControl } from 'react-leaflet';
 import useStyles from "./style";
+import { circleMarker } from 'leaflet';
+
 
 /**
  * @return {null}
  */
+const MARKER_STYLE = {
+  radius: 8,
+  fillColor: "#ff7800",
+  color: "#000",
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 0.8
+};
+
+let map;
 
 function Map(props) {
+
   const classes = useStyles();
   const {data, activeItem} = props;
-
+  const createMapContext = (mapContext) => {
+    map = mapContext;
+  };
+  const marker = (feature, latlng) => {
+    return circleMarker(latlng, MARKER_STYLE)
+  };
+  useEffect(() => {
+    if (!activeItem) return;
+    map.flyTo(activeItem.geometry.coordinates);
+  }, [activeItem]);
   return (
     <div className={classes.container}>
-      <MapContainer center={[55.705, 37.59]} zoom={10} scrollWheelZoom={true} className={classes.containerMap}>
-        <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://tile2.maps.2gis.com/tiles?z={z}&x={x}&y={y}&v=1"
-        />
-        <GeoJSON data={data}/>
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+      <MapContainer
+        whenCreated={createMapContext}
+        center={[55.705, 37.59]}
+        zoom={10}
+        zoomControl={false}
+        scrollWheelZoom={true}
+        className={classes.containerMap}>
+        <LayersControl position="topright">
+          <LayersControl.BaseLayer checked name="2GIS">
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://tile2.maps.2gis.com/tiles?z={z}&x={x}&y={y}&v=1"
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="OpenStreetMap">
+            <TileLayer
+              attribution='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+        </LayersControl>
+
+        <GeoJSON data={data} pointToLayer={marker}>
+
+        </GeoJSON>
+        <ZoomControl position='topright'/>
       </MapContainer>
     </div>
   );
