41eabaa794135507097bf132300c1cd945821093
Владимир Орешин <VSOreshin@sberbank.ru>
2021-10-08 06:29:33 +0300

    fix

--- frontend/src/Components/Map/Map.js
+++ frontend/src/Components/Map/Map.js
@@ -34,9 +34,11 @@ function Map(props) {
         let formatter = new Intl.DateTimeFormat("ru");
         popup += `<div>${alias[key]}: ${formatter.format(date) }</div>`;
       } else if (key === 'address') {
-        popup += `<div>${alias[key]}: ${Object.keys(data[key]).map((prop) => data[key][prop]).join(',')}</div>`
+        popup += `<div>${alias[key]}: ${Object.keys(data[key]).map((prop) => data[key][prop]).join(', ')}</div>`
       } else if (key === 'special') {
         popup += `<div>${alias[key]}: ${data[key] ? 'Да' : 'Нет'}</div>`
+      } else if (key === 'title') {
+        popup += `<div><center><strong>${data[key]}</strong></center></div>`
       } else {
         popup += `<div>${alias[key]}: ${data[key]}</div>`
       }
