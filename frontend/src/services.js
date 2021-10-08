bf6278489e069bc5e5b581e4c6c8cc3811cdee43
fv.tokarev <fv.tokarev@tensor.ru>
2021-10-08 09:54:26 +0300

    -

--- frontend/src/services.js
+++ frontend/src/services.js
@@ -1,11 +1,13 @@
 /**
  * Функция запроса к серверу работает с форматом JSON
  */
+
+const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+};
+
 export const serviceJson = async (address, body, method, errorBackHandler) => {
-    const headers = {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-    };
     let serverAddress = address || '/';
     if (body) {
         serverAddress = `${serverAddress}?`;
@@ -20,4 +22,15 @@ export const serviceJson = async (address, body, method, errorBackHandler) => {
     };
     const response = await fetch(serverAddress, params).catch((e) => {throw e});
     return (response.ok && await response.json()) || (errorBackHandler && errorBackHandler(response));
+};
+
+export const servicePostJson = async (address, body, method, errorBackHandler) => {
+    const serverAddress = address || '/';
+    const params = {
+        method: 'POST',
+        headers: new Headers(headers),
+        body: JSON.stringify(body)
+    };
+    const response = await fetch(serverAddress, params).catch((e) => {throw e});
+    return (response.ok && await response.json()) || (errorBackHandler && errorBackHandler(response)) || false;
 };
