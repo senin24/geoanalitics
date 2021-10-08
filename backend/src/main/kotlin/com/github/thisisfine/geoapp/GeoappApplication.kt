d1147d20047f4b4489b9a474875a3f171c384f42
Pavel Senin <senin24@gmail.com>
2021-10-08 03:56:38 +0300

    Add rest for update Event coordinates

--- backend/src/main/kotlin/com/github/thisisfine/geoapp/GeoappApplication.kt
+++ backend/src/main/kotlin/com/github/thisisfine/geoapp/GeoappApplication.kt
@@ -2,9 +2,11 @@ package com.github.thisisfine.geoapp
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
 
 @SpringBootApplication
+@EnableTransactionManagement
 class GeoappApplication {}
 
 
---
+++ backend/src/main/kotlin/com/github/thisisfine/geoapp/web/CoordinatesDto.kt
@@ -0,0 +1,8 @@
+package com.github.thisisfine.geoapp.web
+
+import java.math.BigDecimal
+
+data class CoordinatesDto(
+    val x: BigDecimal,
+    val y: BigDecimal
+)
